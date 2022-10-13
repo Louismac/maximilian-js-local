696206da33ec1744126bec2dded8188786a57060
Louis Busby <l.mccallum@arts.ac.uk>
2022-10-13 19:28:58 +0100

    own libs

---
+++ server.py
@@ -0,0 +1,24 @@
+# editted from https://gist.github.com/UniIsland/1028b8e98b9963945bbfe37521f9350a
+# Attribution: https://stackoverflow.com/questions/21956683/enable-access-control-on-simple-http-server
+
+
+try:
+    # Python 3
+    from http.server import HTTPServer, SimpleHTTPRequestHandler, test as test_orig
+    import sys
+    def test (*args):
+        test_orig(*args, port=int(sys.argv[1]) if len(sys.argv) > 1 else 4200)
+except ImportError: # Python 2
+    from BaseHTTPServer import HTTPServer, test
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        # self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        # self.send_header('Cross-Origin-Resource-Policy', 'cross-origin')
+        # self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+    test(CORSRequestHandler, HTTPServer)
